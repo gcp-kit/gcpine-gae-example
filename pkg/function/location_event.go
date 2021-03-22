52ea2f378447c72656fdee1d466e96c3e4ed2610
54m <30588003+54m@users.noreply.github.com>
2021-03-23 03:37:53 +0900

    update: add show the name of the called function

--- pkg/function/location_event.go
+++ pkg/function/location_event.go
@@ -3,14 +3,19 @@ package function
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/gcp-kit/line-bot-gcp-go/gcpine"
+	"github.com/go-utils/caller"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"golang.org/x/xerrors"
 )
 
 // LocationEvent - handle location message events
 func LocationEvent(_ context.Context, _ *gcpine.GCPine, event *linebot.Event) ([]linebot.SendingMessage, error) {
+	name := caller.GetCallFuncName()
+	log.Println("Call:", name)
+
 	message, ok := event.Message.(*linebot.LocationMessage)
 	if !ok {
 		return nil, xerrors.New("couldn't cast")
--- pkg/function/text_event.go
+++ pkg/function/text_event.go
@@ -2,14 +2,19 @@ package function
 
 import (
 	"context"
+	"log"
 
 	"github.com/gcp-kit/line-bot-gcp-go/gcpine"
+	"github.com/go-utils/caller"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"golang.org/x/xerrors"
 )
 
 // TextEvent - handle text message events
 func TextEvent(ctx context.Context, pine *gcpine.GCPine, event *linebot.Event) ([]linebot.SendingMessage, error) {
+	name := caller.GetCallFuncName()
+	log.Println("Call:", name)
+
 	message, ok := event.Message.(*linebot.TextMessage)
 	if !ok {
 		return nil, xerrors.New("couldn't cast")
