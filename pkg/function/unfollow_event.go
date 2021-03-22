92622f294a3339ab129363e1b4b0e391f7c75aca
54m <30588003+54m@users.noreply.github.com>
2021-03-23 03:37:13 +0900

    feat: add unfollow event handler

---
+++ pkg/function/unfollow_event.go
@@ -0,0 +1,21 @@
+package function
+
+import (
+	"context"
+	"log"
+
+	"github.com/gcp-kit/line-bot-gcp-go/gcpine"
+	"github.com/go-utils/caller"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+// UnfollowEvent - handle unfollow events
+func UnfollowEvent(_ context.Context, _ *gcpine.GCPine, event *linebot.Event) ([]linebot.SendingMessage, error) {
+	name := caller.GetCallFuncName()
+	log.Println("Call:", name)
+
+	uid := event.Source.UserID
+	log.Println("UID:", uid)
+
+	return nil, nil
+}
