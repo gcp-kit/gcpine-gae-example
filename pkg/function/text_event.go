68087f3ebbbf190cf57651d1ccd4a1986e0b315b
54m <30588003+54m@users.noreply.github.com>
2021-03-23 03:28:03 +0900

    feat: add text message event handler

---
+++ pkg/function/text_event.go
@@ -0,0 +1,56 @@
+package function
+
+import (
+	"context"
+	"log"
+
+	"github.com/gcp-kit/line-bot-gcp-go/gcpine"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"golang.org/x/xerrors"
+)
+
+// TextEvent - handle text message events
+func TextEvent(ctx context.Context, pine *gcpine.GCPine, event *linebot.Event) ([]linebot.SendingMessage, error) {
+	message, ok := event.Message.(*linebot.TextMessage)
+	if !ok {
+		return nil, xerrors.New("couldn't cast")
+	}
+
+	text := message.Text
+
+	replyMessage := linebot.SendingMessage(linebot.NewTextMessage(text))
+
+	if text == "ping" {
+		items := &linebot.QuickReplyItems{
+			Items: []*linebot.QuickReplyButton{
+				{
+					Action: linebot.QuickReplyAction(&linebot.MessageAction{
+						Label: "ping",
+						Text:  "ping",
+					}),
+				},
+			},
+		}
+
+		prof, err := pine.GetProfile(event.Source.UserID).WithContext(ctx).Do()
+		if err != nil {
+			log.Println("Error:", err.Error())
+			return nil, xerrors.Errorf("error in GetProfile method: %w", err)
+		}
+
+		sender := &linebot.Sender{
+			Name:    prof.DisplayName,
+			IconURL: prof.PictureURL,
+		}
+
+		replyMessage = linebot.NewTextMessage("pong").
+			WithQuickReplies(items).
+			WithSender(sender)
+	}
+
+	stack := []linebot.SendingMessage{
+		replyMessage,
+	}
+
+	return stack, nil
+}
