64ad427b1a1748afdad4930fe20be7f3c881e173
54m <30588003+54m@users.noreply.github.com>
2021-03-24 03:27:26 +0900

    clean: delete unnecessary things

--- pkg/constant/constant.go
+++
@@ -1,7 +0,0 @@
-package constant
-
-const (
-	EnvKeyGoogleCloudProject = "GOOGLE_CLOUD_PROJECT" // for Google App Engine
-	EnvKeyServiceName        = "GAE_SERVICE"          // for Google App Engine
-	EnvKeyServiceVersion     = "GAE_VERSION"          // for Google App Engine
-)
